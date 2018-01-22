f5bf9aec49ba7f827b53e32f87a43fec1fabf4b1
char23fj <cfjadin@gmail.com>
2018-01-22 05:43:56 +0000

    Changed higher directory copies back

--- dominion/dominion.h
+++ dominion/dominion.h
@@ -128,14 +128,4 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
 
-int adventurerAction(struct gameState *state);
-
-int smithyAction(struct gameState *state, int handPos);
-
-int tributeAction(struct gameState *state);
-
-int ambassadorAction(struct gameState *state, int handPos, int choice1, int choice2);
-
-int cutpurseAction(struct gameState *state, int handPos);
-
 #endif
